273c40191bbab755077572621f0a641cb3c85f47
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-21 11:42:15 +0000

    Hello World

---
+++ HelloWorld/src/main/java/matthewyeend/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Author:  Matthew Yeend
+ * Project: Hello World
+ * Version: 1.1.1
+ * Date:    ß21/03/2025
+ */
+
+package matthewyeend.helloworld;
+
+/**
+ *
+ * @author matthewyeend
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
