3f2fd51d03e8ef7857aaee8e97103df3e878ca3c
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-30 14:52:23 +0100

    New project

---
+++ SimpleAIAgent/src/main/java/matthewyeend/simpleaiagent/SimpleAIAgent.java
@@ -0,0 +1,21 @@
+/*
+ * Author:  Matthew Yeend
+ * Project: Simple AI Agent
+ * Version: v1.1.1
+ * Date:    30/03/2025
+ */
+
+package matthewyeend.simpleaiagent;
+
+import java.util.Random;
+
+/**
+ *
+ * @author matthewyeend
+ */
+public class SimpleAIAgent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
